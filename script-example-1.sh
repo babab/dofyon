f9704f08c0cfce027d9938841abfa29a44f4fab9
Benjamin Althues <benjamin@babab.nl>
2013-04-21 23:16:01 +0200

    Rename dofyon.sh -> dofyon

--- script-example-1.sh
+++ script-example-1.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
-which dofyon.sh >/dev/null 2>&1
-test $? -eq 0 || (echo dofyon.sh is not in PATH && exit)
+which dofyon >/dev/null 2>&1
+test $? -eq 0 || echo dofyon is not in PATH
 
-dofyon.sh t Testing 123
+dofyon t Testing 123
 sleep 1
-dofyon.sh s DONE
+dofyon s DONE
 
-dofyon.sh t Checking hostname: $(hostname)
-dofyon.sh s d
+dofyon t Checking hostname: $(hostname)
+dofyon s d
 
-dofyon.sh t Testing existence of "'doesnotexist' exec"
+dofyon t Testing existence of "'doesnotexist' exec"
 which doesnotexist >/dev/null 2>&1
-dofyon.sh s dof?
+dofyon s dof?
 
-dofyon.sh t Testing a custom state
-dofyon.sh s CUSTOM
+dofyon t Testing a custom state
+dofyon s CUSTOM
 
-dofyon.sh t Testing a custom state with a custom color
-dofyon.sh e '\033[0;35m''YELLOW'
+dofyon t Testing a custom state with a custom color
+dofyon e '\033[0;35m''YELLOW'
 
-dofyon.sh t Is there a yes/no option
-dofyon.sh s y
+dofyon t Is there a yes/no option
+dofyon s y
 
-dofyon.sh t Can you also show the no option
+dofyon t Can you also show the no option
 sleep 2
-dofyon.sh s n
+dofyon s n
 
-dofyon.sh t "Ok so I ask again, does 'doesnotexist' exist?"
+dofyon t "Ok so I ask again, does 'doesnotexist' exist?"
 which doesnotexist >/dev/null 2>&1
-dofyon.sh s yon?
+dofyon s yon?
